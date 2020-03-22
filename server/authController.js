3d8bb1833fdac8518072cb1163329dddf0d6a8f1
Abaas Khorrami <abaas.khorrami@gmail.com>
2020-03-21 23:02:54 -0400

    Update authController.js
    Cleaned up comments, console.logs

--- server/authController.js
+++ server/authController.js
@@ -9,31 +9,29 @@ const authController = {};
 // Existing User Login Middleware
 authController.verifyExistingUserExists = (req, res, next) => {
   const { username } = req.body;
-  console.log('from login verify middleware: ', username);
   const checkUserSQL = `
     SELECT COUNT(*)
     FROM users
     WHERE username=$1
   `;
+  
   db.query(checkUserSQL, [username], (err, sqlRes) => {
     if (err) return next(err);
     if (sqlRes.rows[0].count === '0') {
-      console.log('user probably does not exist');
       return res.send({ msg: 'Invalid username or password-user' });
     }
-    // if no error and count > 0, username should exist
     return next();
   });
 };
 
 authController.compareHash = (req, res, next) => {
   const { username, password } = req.body;
-  console.log('from hash compare middleware');
   const getHashSQL = `
     SELECT hashed_pw
     FROM users
     WHERE username=$1
   `;
+  
   db.query(getHashSQL, [username], (errSQL, sqlRes) => {
     if (errSQL) return next(errSQL);
     const hash = sqlRes.rows[0].hashed_pw;
@@ -42,24 +40,22 @@ authController.compareHash = (req, res, next) => {
       if (result) return next();
       return res.send({ msg: 'Invalid username or password-hash' });
     });
-    // return here to prevent hanging contenxt?
+    // return here to prevent hanging context?
   });
 };
 
 // New User Registration Middleware
 authController.verifyNewUserDoesNotExist = (req, res, next) => {
   const { username } = req.body;
-  console.log('username from middleware: ', username);
   const checkUserSQL = `
     SELECT COUNT(*)
     FROM users
     WHERE username=$1
   `;
+  
   db.query(checkUserSQL, [username], (err, sqlRes) => {
     if (err) return next(err);
-    console.log(sqlRes.rows[0]);
     if (sqlRes.rows[0].count === '0') {
-      console.log('user probably does not exist');
       return next();
     }
     // short circuit middleware if user already exists
@@ -68,7 +64,6 @@ authController.verifyNewUserDoesNotExist = (req, res, next) => {
 };
 
 authController.generateSaltAndEncrypt = (req, res, next) => {
-  console.log('from encrypter: ', req.body);
   bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
     if (err) return next(err);
     res.locals.hash = hash;
@@ -77,7 +72,6 @@ authController.generateSaltAndEncrypt = (req, res, next) => {
 };
 
 authController.createUserRecord = (req, res, next) => {
-  console.log('from record creator');
   const { username } = req.body;
   const { hash } = res.locals;
   const newUserSQL = `
@@ -90,10 +84,9 @@ authController.createUserRecord = (req, res, next) => {
       BTC_balance
     ) VALUES ($1, $2, $3, $4, $5, $6)
   `;
+  
   db.query(newUserSQL, [new Date(), username, hash, hash, 100000, 0], (err, sqlRes) => {
     if (err) return next(err);
-    console.log('user creation was successful');
-    // console.log(sqlRes);
     next();
   });
 };
@@ -105,6 +98,7 @@ authController.getPortfolio = (req, res, next) => {
     FROM users
     WHERE username=$1
   `;
+  
   db.query(getPortfolioSQL, [username], (err, sqlRes) => {
     if (err) return next(err);
     res.locals.portfolio = sqlRes.rows;
